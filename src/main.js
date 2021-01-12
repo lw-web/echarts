32ab60dbdad1d96f2e20ca54497b2689a2c41151
anhong123 <980123389@qq.com>
2021-01-12 09:33:12 +0800

    123

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import store from './store'
 import './css/index.scss'
 import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.css'
+// 水球图表插件版本较低 4.8.0 引用时需注意是否和当前项目Ecahrts版本一致
 import 'echarts-liquidfill'
 
 const echarts = require('echarts')
